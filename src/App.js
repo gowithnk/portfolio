aa94afce22fd19c21877240f20f004e26b36a72c
Niranjan kumar <gowithnk@gmail.com>
2020-09-25 15:07:43 +0530

    Title message and scroll down button added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import MyNavbar from "./components/navbar/navbar";
+import MyCarousel from "./components/carousel/carousel";
+import MyTitleMessage from "./components/title-message/titleMessage";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
-      <h1>Portfolio</h1>
-      <p>Niranjan</p>
-      <p>Niranjan</p>
-      <p>Niranjan</p>
+      <MyCarousel />
+      <MyTitleMessage />
     </div>
   );
 }
